0b955b7f46f575eb28108a040ce196b761f4dd3b
sagae keiga <sagae5.28rujeae@gmail.com>
2017-02-18 14:37:06 +0000

    working_directory

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 worker_processes 2
-working_directory "/home/anime/deploy/s_press"
+working_directory "/home/anime/bot/current"
 
 litsen "/var/run/unicorn/unicorn.sock"
 pid "/var/run/unicorn/unicorn.pid"
