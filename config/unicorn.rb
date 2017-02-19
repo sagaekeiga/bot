6c8b36544fe26402ffa7728f09c12d7d411fefd0
sagae keiga <sagae5.28rujeae@gmail.com>
2017-02-19 03:29:58 +0000

    working_directory

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 worker_processes 2
 working_directory "/home/anime/bot/current"
 
-listen "/var/run/unicorn/unicorn.sock"
-pid "/var/run/unicorn/unicorn.pid"
+listen "/var/run/tmp/unicorn.sock"
+pid "/var/run/tmp/unicorn.pid"
 
 preload_app true
